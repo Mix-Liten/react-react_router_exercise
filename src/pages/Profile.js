703c6c857969386aaaf8e92bbf054ae920899bc3
Mix-Liten <mixliten@gmail.com>
2021-07-11 17:31:59 +0800

    feat: Update useRouteMatch set nested routing

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,36 @@
 import { useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import { Switch, Route, Link, useHistory, useRouteMatch } from 'react-router-dom'
 
 const Profile = ({ isLogin }) => {
   const history = useHistory()
+  const { path, url } = useRouteMatch()
   useEffect(() => {
     if (!isLogin) history.push('/')
   }, [isLogin, history])
   return (
-    <h1>
-      Profile Page
-    </h1>
+    <>
+      <h1>
+        Profile Page
+      </h1>
+
+      <ul className="profile-link">
+        <li>
+          <Link to={`${url}/view`}>View Profile</Link>
+        </li>
+        <li>
+          <Link to={`${url}/edit`}>Edit Profile</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route path={`${path}/view`}>
+          <h2>View Profile Page</h2>
+        </Route>
+        <Route path={`${path}/edit`}>
+          <h2>Edit Profile Page</h2>
+        </Route>
+      </Switch>
+    </>
   )
 }
 
