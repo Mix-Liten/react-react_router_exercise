f8e5eb5d26a266d937388cf3db0bb8a33d9574d2
Mix-Liten <mixliten@gmail.com>
2021-07-11 16:43:57 +0800

    feat: Update useLocation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,16 +9,24 @@ const Home = () => {
       </h1>
       <ul className="post-link">
         <li>
-          <Link to="/post/Vivy">Post ID: Vivy</Link>
-        </li>
-        <li>
           <Link to="/post/Matsumoto">Post ID: Matsumoto</Link>
         </li>
         <li>
-          <Link to="/post/Estella">Post ID: Estella</Link>
+          {/* to prop 連結位置，字串寫法 */}
+          <Link to={`/post/Vivy?Song=${encodeURI("Fluorite Eye's Song")}`}>Post ID: Vivy</Link>
         </li>
         <li>
-          <Link to="/post/Grace">Post ID: Grace</Link>
+          {/* to prop 連結位置，物件寫法 */}
+          <Link to={{
+            pathname: "/post/Estella",
+            search: `?Song=${encodeURI("Ensemble for Polaris")}`,
+          }}>Post ID: Estella</Link>
+        </li>
+        <li>
+          <Link to={{
+            pathname: "/post/Grace",
+            search: `?Song=${encodeURI("Sing My Pleasure (Grace Version)")}`,
+          }}>Post ID: Grace</Link>
         </li>
       </ul>
     </>
--- src/pages/Post/_id.js
+++ src/pages/Post/_id.js
@@ -1,4 +1,5 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useLocation } from 'react-router-dom'
+import '../../App.css'
 
 // // get params by prop
 // const _id = ({ match }) => {
@@ -12,10 +13,17 @@ import { useParams } from 'react-router-dom'
 // get params by hook
 const _id = () => {
   const { id } = useParams()
+  // get query by hook
+  const query = new URLSearchParams(useLocation().search)
   return (
-    <h2>
-      Id = {id}
-    </h2>
+    <>
+      <h2>
+        Id = {id}
+      </h2>
+      {!!query.toString().length && (<h2 className="query-content">
+        Query = {Array.from(query.keys()).map(key => (<p key={`${key}-${query.get(key)}`}>{key}: {query.get(key)}</p>))}
+      </h2>)}
+    </>
   )
 }
 
